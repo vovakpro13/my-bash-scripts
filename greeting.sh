22995f43f3e135012fb529a8f7f1ec2efc37223c
Volodymyr Melnyk <vovakpro13@gmail.com>
2022-05-04 09:19:19 +0300

    pwd, date, whoami

--- greeting.sh
+++ greeting.sh
@@ -3,6 +3,9 @@
 name=$1
 compliment=$2
 
+user=$(whoami)
+date=$(date)
+path=$(pwd)
 
 echo "Hi, there!"
 
@@ -10,3 +13,7 @@ sleep 1
 
 echo "Have a nice day, $name!"
 echo "You have the best $compliment, I've ever seen $name!!"
+sleep 2
+
+
+echo "You are already logged in as '$user' and you are in the directory: $path. Also today is $date"
